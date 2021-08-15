a4ddf2bf75f19f1e43cce8423a9dfe02444a3bf8
Laetian <laetianv@gmail.com>
2021-08-15 16:47:56 +0200

    Added Gizmos to Sight
    Sphere for range of  vision
    Lines for angle of vision
    Line red in range but obstacle, green detected no obstacles, on sight

--- Assets/Scripts/Game/AI/Enemy/Sight.cs
+++ Assets/Scripts/Game/AI/Enemy/Sight.cs
@@ -35,11 +35,16 @@ public class Sight : MonoBehaviour
                 //TODO:Object Inside the vision angle
 
                 //Check there aren't obstacles between enemy and objective 
-                if(!Physics.Linecast(transform.position, collider.bounds.center, obstacleLayers))
+                if(!Physics.Linecast(transform.position, collider.bounds.center, out RaycastHit hit, obstacleLayers))
                 {
+                    Debug.DrawLine(transform.position, collider.bounds.center, Color.green);
                     //Save the reference of detected target
                     detectedTarget = collider;
                     break;//Avoiding the enemy becomes crazy between targets in case more than 1 in sight
+                }
+                else
+                {
+                    Debug.DrawLine(transform.position, hit.point, Color.red);
                 }
             }
         }
@@ -48,5 +53,15 @@ public class Sight : MonoBehaviour
             Collider collider = colliders[i];
             //TODO: Do things to collider
         }*/
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, distance);
+        Gizmos.color = Color.magenta;
+        Vector3 rightDir = Quaternion.Euler(x: 0, y: angle, z: 0) * transform.forward;
+        Gizmos.DrawRay(transform.position, rightDir * distance);
+        Vector3 leftDir = Quaternion.Euler(x: 0, y: -angle, z: 0) * transform.forward;
+        Gizmos.DrawRay(transform.position, leftDir * distance);
     }
 }
