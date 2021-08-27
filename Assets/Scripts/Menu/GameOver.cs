a0325f4190407fd1b4db7c2bf0068f50097786af
Laetian <laetianv@gmail.com>
2021-08-27 20:56:52 +0200

    Añadido PlayerPref max score

--- Assets/Scripts/Menu/GameOver.cs
+++ Assets/Scripts/Menu/GameOver.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
-        
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ReloadScene()
     {
-        
+        SceneManager.LoadScene("Level1");
     }
 }
