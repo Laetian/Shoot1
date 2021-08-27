085e8c5232442deb14c6734fbd2e168c0ac7e6e5
Laetian <laetianv@gmail.com>
2021-08-27 21:02:10 +0200

    Added PlayerPrefs TimeRecord

--- Assets/Scripts/Game/GameModeWaves.cs
+++ Assets/Scripts/Game/GameModeWaves.cs
@@ -22,7 +22,8 @@ public class GameModeWaves : MonoBehaviour
 
     void CheckLoseCondition()
     {
-    //PERDER
+        //PERDER
+        RegisterScore();
     SceneManager.LoadScene("LoseScene");      
     }
     void CheckWinCondition()
@@ -30,7 +31,30 @@ public class GameModeWaves : MonoBehaviour
         //GANAR
         if (EnemyManager.SharedInstance.EnemyCount <= 0 && WaveManager.SharedInstance.WaveCount <= 0)
         {
+            RegisterScore();
             SceneManager.LoadScene("WinScene");
         }
     }
+    void RegisterScore()
+    {
+        var actualScore = ScoreManager.SharedInstance.Amount;
+        PlayerPrefs.SetInt("Last Score", actualScore);
+
+        var highScore = PlayerPrefs.GetInt("High Score", 0);
+        if (actualScore > highScore)
+        {
+            PlayerPrefs.SetInt("High Score", actualScore);
+        }
+    }
+    /*void RegisterTime() //En caso de querer registrar un record por tiempo
+    {
+        var actualTime = Time.time;
+        PlayerPrefs.SetFloat("Last Time", actualTime);
+
+        var lowestTime = PlayerPrefs.GetFloat("Lowest Time", 99999.0f);
+        if (actualTime < lowestTime)
+        {
+            PlayerPrefs.SetFloat("Lowest Time", actualTime);
+        }
+    }*/
 }
